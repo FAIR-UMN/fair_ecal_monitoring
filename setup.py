933419c9dea642c77a3a3b3264dc139d8348030a
bmjoshi <joshi.bm163@gmail.com>
2021-09-29 11:15:11 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository is for development of ECAL crystal monitoring prediction as a part of FAIR project.',
+    author='FAIR4HEP',
+    license='MIT',
+)
